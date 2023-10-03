780ca3605286f671e49caf4ffe5c023bf859ef22
RAABIN SINGH  S A <112077212+raabin10@users.noreply.github.com>
2023-10-03 13:07:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,20 +1,16 @@
-//your JS code here. If required.
-// JavaScript to dynamically create 799 more square boxes
-const container = document.getElementById('color-board');
+const container = document.getElementById('container');
+const squares = 800;
 
-for (let i = 0; i < 799; i++) {
-    const square = document.createElement('div');
-    square.className = 'square';
-    container.appendChild(square);
+for (let i = 0; i < squares; i++) {
+  const innerDiv = document.createElement('div');
+  innerDiv.className = "square";
+  container.appendChild(innerDiv);
+
+  innerDiv.addEventListener('mouseover', () => {
+    innerDiv.style.animation = "changing 1s linear forwards";
+  });
+
+  innerDiv.addEventListener('mouseout', () => {
+    innerDiv.style.animation = "";
+  });
 }
-
-// Optional: Reset the color after 1 second
-const squares = document.querySelectorAll('.square');
-
-squares.forEach((square) => {
-    square.addEventListener('mouseover', () => {
-        setTimeout(() => {
-            square.style.backgroundColor = '#ccc'; // Reset to original color
-        }, 1000);
-    });
-});
